6a73453e646b492eab1d6fb5a10b964114b24b09
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-10-30 12:07:52 -0500

    Excercise 5, Inverted number

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-#Ejercicio 4
+#Ejercicio 5
 
-#Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
+#Escriba un programa que pida al usuario un entero de tres dígitos, y entregue el número con los dígitos en orden inverso:
 
-#Ingrese longitud: 45
-#45 cm = 17.7165 in
-#Ingrese longitud: 13
-#13 cm = 5.1181 in
+#Ingrese numero: 345
+#543
+#Ingrese numero: 241
+#142
 
-cm = float(input("Enter the lenght: "))
-inches = ((cm) / 2.54)
-
-print(f"""That is equal to : {inches} inches""")
+number = int(input("Enter a three-digit number: "))
+number2 = str(number)[::-1]
+print(f""" The invert number is: {number2}""")
