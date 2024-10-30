250593eb8bcda1ba7aa1f5e867a2c182ff837055
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-10-30 12:22:27 -0500

    Excercise 6, Hypotenuse

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-#Ejercicio 5
+#Ejercicio 6
 
-#Escriba un programa que pida al usuario un entero de tres dígitos, y entregue el número con los dígitos en orden inverso:
+#Escriba un programa que reciba como entrada las longitudes de los dos catetos a y b
+#de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa c
+#del triangulo, dado por el teorema de Pitágoras: c2=a2+b2.
 
-#Ingrese numero: 345
-#543
-#Ingrese numero: 241
-#142
+#Ingrese cateto a: 7
+#Ingrese cateto b: 5
+#La hipotenusa es 8.6023252670426267
 
-number = int(input("Enter a three-digit number: "))
-number2 = str(number)[::-1]
-print(f""" The invert number is: {number2}""")
+import math
+
+cathethus1 = float(input("Enter the first lenght: "))
+cathethus2 = float(input("Enter the second lenght: "))
+
+hypotenuse = math.sqrt ( (cathethus1) ** 2 + (cathethus2) ** 2 )
+
+print(f"""The hypotenuse is: {hypotenuse} """)
