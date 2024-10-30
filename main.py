33097da83a19067180f6a5152d84c4dbd9cf1d1a
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-10-30 12:38:24 -0500

    Excercise 7, Future Hour

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-#Ejercicio 6
+#Ejercicio 7
 
-#Escriba un programa que reciba como entrada las longitudes de los dos catetos a y b
-#de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa c
-#del triangulo, dado por el teorema de Pitágoras: c2=a2+b2.
+#Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, que indique qué hora marcará
+#el reloj dentro de h horas:
 
-#Ingrese cateto a: 7
-#Ingrese cateto b: 5
-#La hipotenusa es 8.6023252670426267
+#Hora actual: 3
+#Cantidad de horas: 5
+#En 5 horas, el reloj marcara las 8
+#Hora actual: 11
+#Cantidad de horas: 43
+#En 43 horas, el reloj marcara las 6
 
-import math
 
-cathethus1 = float(input("Enter the first lenght: "))
-cathethus2 = float(input("Enter the second lenght: "))
+localhour = int(input("What time is it?: "))
+advancehour = int(input("How many hours do you wanna advance: "))
 
-hypotenuse = math.sqrt ( (cathethus1) ** 2 + (cathethus2) ** 2 )
+futurehour = (localhour + advancehour) % 24
 
-print(f"""The hypotenuse is: {hypotenuse} """)
+print(f"""The future hour is going to be: {futurehour}""")
