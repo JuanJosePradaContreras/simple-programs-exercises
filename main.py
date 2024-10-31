b54b23fad4a7f08842dd996c1d4ed2096a587f57
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-10-31 11:19:49 -0500

    Excercise 10, perfect egg

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
-#Ejercicio 9
+#Ejercicio 10
 
-#Un alumno desea saber que nota necesita en el tercer certamen para aprobar un ramo.
+#Cuando un huevo es hervido en agua, las proteínas comienzan a coagularse cuando la temperatura sobrepasa un punto crítico. A medida que la temperatura 
+#aumenta, las reacciones se aceleran.
 
-#El promedio del ramo se calcula con la siguiente formula.
+#En la clara, las proteínas comienzan a coagularse para temperaturas sobre 63°C, mientras que en la yema lo hacen para temperaturas sobre 70°C. 
+#Para hacer un huevo a la copa, la clara debe haber sido calentada lo suficiente para coagularse a más de 63°C, pero la yema no debe sobrepasar los 
+#70°C para evitar obtener un huevo duro.
 
-#NC=(C1+C2+C3)3
-#NF=NC⋅0.7+NL⋅0.3
+#El tiempo en segundos que toma al centro de la yema alcanzar Ty, °C está dado por la fórmula:
 
-#Donde NC es el promedio de certámenes, NL el promedio de laboratorio y NF la nota final.
+#t=M2/3cρ1/3Kπ2(4π/3)2/3ln[0.76To−TwTy−Tw], donde M es la masa del huevo, ρ su densidad, c su capacidad calorífica específica 
+#y K su conductividad térmica. Algunos valores típicos son:
 
-#Escriba un programa que pregunte al usuario las notas de los dos primeros certamen y la nota de laboratorio, y muestre la nota que necesita el alumno para aprobar el ramo con nota final 60.
+#M=47[g] para un huevo pequeño y M=67[g] para uno grande,
+#ρ=1.038[gcm−3],
+#c=3.7[Jg−1K−1], y
+#K=5.4⋅10−3[Wcm−1K−1].
+#Tw es la temperatura de ebullición del agua y To la temperatura original del huevo antes de meterlo al agua, ambos en grados Celsius.
 
-#Ingrese nota certamen 1: 45
-#Ingrese nota certamen 2: 55
-#Ingrese nota laboratorio: 65
-#Necesita nota 72 en el certamen 3
+#Escriba un programa que reciba como entrada la temperatura original del huevo y muestre como salida el tiempo en segundos que le toma alcanzar la temperatura máxima para prepararlo a la copa.
+
+import math
+
+original_temp_egg = float(input("Original temperature from the egg\n"))
+boiling_temp_wat= 100 
+yolk_temp_coagulate = 70 
+egg_mass= 67 
+density_from_egg= 1.038 
+cap_cal_egg= 3.7 
+cond_term_egg = 5.4 * math.pow(10, -3) 
+dividing = math.pow(egg_mass, (2/3)) * (cap_cal_egg * (math.pow(density_from_egg, (1/3))))
+divider = (cond_term_egg * math.pow(math.pi, 2)) * math.pow((4*math.pi) / 3, (2/3))
+result = dividing / divider
+
+second_result = math.log(0.76 * ((original_temp_egg - boiling_temp_wat) / (yolk_temp_coagulate - boiling_temp_wat)))
+
+seconds = result * second_result
 
 
-grade_1 = float(input("What was your grade in the first contest?: "))
-grade_2 = float(input("What was your grade in the second contest?: "))
-lab_grade = float(input("What was your grade in the lab? :"))
-
-final_note= 60
-
-lb = lab_grade
-
-grade_necesary = (final_note - lb * 0.3) / 0.7
-
-grade_3 = grade_necesary * 3 - grade_1 - grade_2
-
-print(f""" You need {grade_3 :.2f} in the third contest to pass""")
+print(f"The max time to prepare the egg is {seconds :.2f} seconds")
