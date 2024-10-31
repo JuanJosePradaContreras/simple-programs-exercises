dc7d873dc3ad74f923c3106fc168e96357e04ce9
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-10-31 10:40:01 -0500

    Excercise 8, Decimal part

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-#Ejercicio 7
+#Ejercicio 8
 
-#Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, que indique qué hora marcará
-#el reloj dentro de h horas:
+#Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
 
-#Hora actual: 3
-#Cantidad de horas: 5
-#En 5 horas, el reloj marcara las 8
-#Hora actual: 11
-#Cantidad de horas: 43
-#En 43 horas, el reloj marcara las 6
+#Ingrese un numero: 4.5
+#0.5
+#Ingrese un numero: -1.19
+#0.19
 
 
-localhour = int(input("What time is it?: "))
-advancehour = int(input("How many hours do you wanna advance: "))
+number = float(input("Enter your number: "))
 
-futurehour = (localhour + advancehour) % 24
+decimal_part =  number - int(number)
 
-print(f"""The future hour is going to be: {futurehour}""")
+print(f"""The decimal part from your number is: {decimal_part :.2f}""")
